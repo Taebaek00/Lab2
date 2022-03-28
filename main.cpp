a952a8abd82db6fda59f80e0e0cdcb6f51e0ed3c
leejunhee <imjun0304@naver.com>
2022-03-28 21:26:37 +0900

    edited

--- main.cpp
+++ main.cpp
@@ -142,7 +142,15 @@ void bubble_sort_descending(int* array, unsigned int size) {
     {
         //#######################################################
         //Implement here
-        ;
+        for (unsigned int j=0; j <size - 1-i;j++){
+            int temp;
+            if (array[j]<array[j+1]){
+                temp=array[j+1];
+                array[j+1]=array[j];
+                array[j]=temp;
+                }
+        }
+     
         //#######################################################
     }
 }
@@ -152,20 +160,37 @@ void insertion_sort_descending(int* array, unsigned int size) {
 	{
 		//#######################################################
         //Implement here
-        ;
+         	int temp=array[i];
+                int j=i-1;
+                while (1){
+                	if (j<0) break;
+               		if (array[j]>=temp) break;
+                	array[j+1]=array[j];
+                	j--;
+		}
+	array[j+1]=temp;
         //#######################################################
 	}
+		
+
 }
 
 void selection_sort_descending(int* array, unsigned int size) {
 	for (unsigned int i = 0; i < size - 1; i++)
 	{
-		//#######################################################
-        //Implement here
-        ;
-        //#######################################################
+	    int max=array[i];
+            int max_index=i;
+       	    for (unsigned int j=i+1;j<size;j++){
+           	 if (array[j]>max){
+                	max=array[j];
+                	max_index=j;
+            }
+        }
+    swap(array[i],array[max_index]);
 	}
-}
+}		
+
+
 
 
 
